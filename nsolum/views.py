0a38c6a4b987469929f460676bd3fcabfe81a8a9
Artem <artem@Yuris-MacBook-Air.local>
2020-06-10 17:32:49 +0200

    api search

--- nsolum/views.py
+++ nsolum/views.py
@@ -61,15 +61,34 @@ def login(request):
         cursor = cnx.cursor()
         text = '''SELECT id FROM users WHERE email = "{}" AND password = "{}"'''.format(email, password)
         cursor.execute(text)
-        if type(cursor.fetchall()[0][0]) == int:
-            return redirect('http://0.0.0.0:8080/welcome_page')                 
+        try: 
+            type(cursor.fetchall()[0][0])
+            request.session['login'] = True
+            request.session['id'] = cursor.fetchall()[0][0]
+            return redirect('http://0.0.0.0:8080/spheres')
+        except IndexError:
+            return redirect('http://0.0.0.0:8080/login')                 
     return render(request, 'login.html')
 
 def spheres(request):
     return render(request, 'spheres.html')
 
 def znakomstva(request):
-    return render(request, 'znakomstva.html')
+    if request.session['login']:
+        return render(request, 'znakomstva.html', {'idha': request.session['id']})
+    else:
+        return render(request, 'login.html')
 
 def test(request):
     return render(request, 'test.html')
+
+def api_search_meets(request, idha, from_years, to_years):
+    cnx = mysql.connector.connect(user='root', password='root',host='docker.for.mac.localhost',database='znakomstva')
+    cursor = cnx.cursor()
+    text = ''' select name, instagram, facebook, vkontakte from users where (
+                (YEAR(CURRENT_DATE) - YEAR(birthday)) -                            
+                (DATE_FORMAT(CURRENT_DATE, '%m%d') < DATE_FORMAT(birthday, '%m%d'))
+            ) <= {} and {} <= (
+                (YEAR(CURRENT_DATE) - YEAR(birthday)) -                            
+                (DATE_FORMAT(CURRENT_DATE, '%m%d') < DATE_FORMAT(birthday, '%m%d'))
+            ) and city_id = {} and lang_id = {};  '''
